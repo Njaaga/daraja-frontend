229810976ff1eb0caef484acaf9c283563bc1975
Njaaga <jn.njaaga@gmail.com>
2026-01-01 11:15:10 -0500

    Update page.jsx

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000";
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://daraja-frontend-dl85.vercel.app";
 
 export default function ClientSignup() {
   const router = useRouter();
