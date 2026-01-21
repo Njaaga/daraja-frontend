769033041e58ceaef734901e907eab9ab8c6e53a
Njaaga <jn.njaaga@gmail.com>
2026-01-21 13:54:02 -0500

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -6,7 +6,8 @@ import axios from "axios";
 import { saveTokens, saveTenant, saveUserRole } from "@/lib/apiClient";
 import Link from "next/link";
 
-const API_BASE = "https://darajatechnologies.ca";
+const API_BASE =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
 
 export default function LoginPage() {
   const router = useRouter();
