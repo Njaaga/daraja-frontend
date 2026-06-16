44ba18e6ba200b36cb18642d9ef7ec0451f45f6b
Njaaga <jn.njaaga@gmail.com>
2026-06-16 06:57:15 -0400

    Update page.jsx

--- app/executive/page.jsx
+++ app/executive/page.jsx
@@ -1,1 +1,15 @@
+export default function ExecutiveDashboard() {
+  return (
+    <div className="space-y-6">
+      <h1>Executive Dashboard</h1>
 
+      <KpiGrid />
+
+      <RevenueTrend />
+
+      <InsightsPanel />
+
+      <AlertsPanel />
+    </div>
+  );
+}
