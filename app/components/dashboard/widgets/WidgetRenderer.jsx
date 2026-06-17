05e731ad175dda0d83eaaacebc178a92a1136bf0
Njaaga <jn.njaaga@gmail.com>
2026-06-17 17:25:26 -0400

    Update WidgetRenderer.jsx

--- app/components/dashboard/widgets/WidgetRenderer.jsx
+++ app/components/dashboard/widgets/WidgetRenderer.jsx
@@ -1,1 +1,24 @@
+"use client"
+import KPICard from "./KPICard";
+import GaugeCard from "./GaugeCard";
+import AIInsightCard from "./AIInsightCard";
 
+export default function WidgetRenderer({ widget }) {
+  switch (widget.type) {
+    case "kpi":
+      return <KPICard widget={widget} />;
+
+    case "gauge":
+      return <GaugeCard widget={widget} />;
+
+    case "insight":
+      return <AIInsightCard widget={widget} />;
+
+    default:
+      return (
+        <div className="rounded-xl border p-4">
+          Unknown widget type: {widget.type}
+        </div>
+      );
+  }
+}
