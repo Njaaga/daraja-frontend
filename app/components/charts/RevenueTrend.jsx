3e4ea8a163c58447fcb8b6eab106a1c217d66f44
Njaaga <jn.njaaga@gmail.com>
2026-06-19 11:28:23 -0400

    Update RevenueTrend.jsx

--- app/components/charts/RevenueTrend.jsx
+++ app/components/charts/RevenueTrend.jsx
@@ -3,35 +3,28 @@
 import {
   LineChart,
   Line,
-  ResponsiveContainer,
   XAxis,
   YAxis,
   Tooltip,
+  ResponsiveContainer,
 } from "recharts";
 
-export default function RevenueTrend() {
-
-  const data = [
-    { month: "Jan", revenue: 100000 },
-    { month: "Feb", revenue: 115000 },
-    { month: "Mar", revenue: 128000 },
-    { month: "Apr", revenue: 142000 },
-    { month: "May", revenue: 151000 },
-  ];
-
+export default function RevenueTrend({
+  data,
+}) {
   return (
     <ResponsiveContainer
       width="100%"
       height={300}
     >
       <LineChart data={data}>
-        <XAxis dataKey="month" />
+        <XAxis dataKey="date" />
         <YAxis />
         <Tooltip />
 
         <Line
           type="monotone"
-          dataKey="revenue"
+          dataKey="value"
         />
       </LineChart>
     </ResponsiveContainer>
