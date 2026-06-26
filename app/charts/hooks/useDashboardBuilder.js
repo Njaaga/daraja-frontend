e7c659a1cfd403702a8e6c9cec28be46cd47b033
Njaaga <jn.njaaga@gmail.com>
2026-06-26 12:52:10 -0400

    Update useDashboardBuilder.js

--- app/charts/hooks/useDashboardBuilder.js
+++ app/charts/hooks/useDashboardBuilder.js
@@ -4,11 +4,20 @@ export default function useDashboardBuilder() {
   const [dashboardName, setDashboardName] = useState("");
   const [dashboardId, setDashboardId] = useState(null);
 
+  const [datasets, setDatasets] = useState([]);
+  const [selectedDatasets, setSelectedDatasets] = useState([]); // dataset objects
+
   return {
     dashboardName,
     setDashboardName,
 
     dashboardId,
     setDashboardId,
+
+    datasets,
+    setDatasets,
+
+    selectedDatasets,
+    setSelectedDatasets,
   };
 }
